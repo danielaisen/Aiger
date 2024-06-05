122946fe7c25036c49af47941bf89c463e514f13
Daniel Bachmann Aisen <danielmaxaisen@gmail.com>
2024-06-05 18:27:18 +0200

    package modified

--- Package.swift
+++ Package.swift
@@ -3,7 +3,16 @@ import PackageDescription
 
 let package = Package(
     name: "Aiger",
+    products: [.library(name: "Aiger", targets: ["Aiger"]), ],
     dependencies: [
-        .package(url: "https://github.com/danielaisen/CAiger.git", from: "1.0.0")
-    ]
+        .package(url: "https://github.com/danielaisen/CAiger.git", from: "1.0.1")
+    ],
+    targets: [        
+        .target(
+            name: "Aiger", 
+            dependencies: ["CAiger"],
+            path: "Sources"),
+        .testTarget(
+            name: "AigerTests", 
+            dependencies: ["Aiger"])]
 )
